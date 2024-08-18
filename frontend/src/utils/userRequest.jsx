46dcae9add0d6a8fe1031c97795f652d45e15112
HasnainAhmad0022 <hasnainahmad0022@gmail.com>
2024-08-18 16:12:31 +0500

    Commit

--- frontend/src/utils/userRequest.jsx
+++ frontend/src/utils/userRequest.jsx
@@ -1,18 +1,46 @@
 
+// import axios from "axios";
+// import { baseUrl } from './config.jsx';
+// const memberDataString = sessionStorage.getItem('slicUserData');
+// const getToken = JSON.parse(memberDataString);
+// console.log(getToken)
+
+// const newRequest = axios.create({
+//     baseURL: baseUrl,
+//     // withCredentials: true,
+//     headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${getToken?.data?.token}`,
+//     }
+
+// });
+
+// export default newRequest;
+
+
 import axios from "axios";
 import { baseUrl } from './config.jsx';
-const memberDataString = sessionStorage.getItem('slicUserData');
-const getToken = JSON.parse(memberDataString);
-console.log(getToken)
 
+// Create the Axios instance
 const newRequest = axios.create({
     baseURL: baseUrl,
-    // withCredentials: true,
     headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${getToken?.data?.token}`,
+    },
+});
+
+// Use Axios request interceptor to set Authorization header dynamically
+newRequest.interceptors.request.use((config) => {
+    const memberDataString = sessionStorage.getItem('slicUserData');
+    const getToken = JSON.parse(memberDataString);
+
+    if (getToken?.data?.token) {
+        config.headers.Authorization = `Bearer ${getToken.data.token}`;
     }
 
+    return config;
+}, (error) => {
+    return Promise.reject(error);
 });
 
 export default newRequest;
