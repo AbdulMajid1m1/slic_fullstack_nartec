61c8b4d941fecb3eccca94735c9b7166d1216dd3
Wasim Zaman <wasimxaman13@gmail.com>
2024-08-20 15:47:48 +0500

    chore: Move getLineItemsByHeadSysId route to its correct position

--- backend/routes/TblIPOFPODetails.js
+++ backend/routes/TblIPOFPODetails.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const controller = require("../controllers/TblIPOFPODetails");
 const isAuth = require("../middleware/is-auth");
 
-router.get("/v1/:headSysId", controller.getLineItemsByHeadSysId);
 router.post("/v1/fetchByMultipleIds", controller.fetchByMultipleIds);
 
 // Route to create a new line item
@@ -12,6 +11,8 @@ router.post("/v1/create", controller.createLineItem);
 
 // Route to get all line items
 router.get("/v1/getAll", controller.getAllLineItems);
+
+router.get("/v1/:headSysId", controller.getLineItemsByHeadSysId);
 
 // Route to update a line item by HEAD_SYS_ID and ITEM_CODE
 router.put("/v1/update/:headSysId/:itemCode", controller.updateLineItem);
