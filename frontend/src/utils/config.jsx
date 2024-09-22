0f42d76d34f39839eb053c45fce1c54d783b2f78
unknown <abdulmajid1m2@gmail.com>
2024-09-22 19:43:17 +0500

    adds

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -2,7 +2,7 @@
 // let mainUrl = 'http://gs1ksa.org:8080';
 // let mainUrl = 'https://slicapp.online:8082';
 
-// ... For Development .. //
+// ... For Development ... //
 let mainUrl = "https://uat.slicapp.online";
 
 // ... for PRODUCTION ... //
